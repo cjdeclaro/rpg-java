3fa15cdd165bb73f1a6ebe967785254fad746e1e
cjdeclaro <cjdeclaro16@gmail.com>
2024-06-26 17:52:58 +0800

    Update on the Frame title

--- src/Home.java
+++ src/Home.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class Home {
 	public static void main(String[] a) {
-		JFrame frmMain = new JFrame("RPG");
+		JFrame frmMain = new JFrame("RPG Dynamic");
 		
 		frmMain.setSize(700, 700);
 		frmMain.setVisible(true);
